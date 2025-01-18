acf1c5d3f9f15a249a350b9d5c293363b48a6f5a
BugHunter-X <askzenai@gmail.com>
2025-01-18 12:10:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,26 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Component mounted');
+    return () => {
+      console.log('Component unmounted');
+    };
+  }, []);
+
+  const handleClick = () => {
+    setTimeout(() => {
+      setCount(prevCount => prevCount + 1);
+      console.log('Count updated');
+    }, 1000);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+}
+```
